17843fcb23e95fb9afa37e46cfe66c5c708c5d97
Thomas Clarke <thomas.clarke@anordmardix.com>
2019-04-16 22:48:28 +0100

    first commit

---
+++ src/Entity/Orders.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\OrdersRepository")
+ */
+class Orders
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $placedby;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Details;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlacedby(): ?string
+    {
+        return $this->placedby;
+    }
+
+    public function setPlacedby(string $placedby): self
+    {
+        $this->placedby = $placedby;
+
+        return $this;
+    }
+
+    public function getDetails(): ?string
+    {
+        return $this->Details;
+    }
+
+    public function setDetails(string $Details): self
+    {
+        $this->Details = $Details;
+
+        return $this;
+    }
+}
